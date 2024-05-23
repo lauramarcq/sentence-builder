6d611d7320115185a571ec16e04822b476431acd
Laura Valdivia <lauravaldivia@Lauras-MBP.ultrahub>
2024-05-23 18:28:22 +0100

    fix form styles

--- src/components/CardInput.vue
+++ src/components/CardInput.vue
@@ -1,43 +1,96 @@
 <template>
-    <div>
-        <form>
-  <div class="space-y-12">
-    <div class="border-b border-gray-900/10 pb-12">
-      <h2 class="text-base font-semibold leading-7 text-gray-900">Instructions</h2>
-      <p class="mt-1 text-sm leading-6 text-gray-600">Choose one word for Subject, one or two words for Verb, and one word for Object. When you hit submit it will generate a grammatically correct sentence.</p>
-
-      <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-        <div class="sm:col-span-4">
-          <label for="username" class="block text-sm font-medium leading-6 text-gray-900">Username</label>
-          <div class="mt-2">
-            <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-              <span class="flex select-none items-center pl-3 text-gray-500 sm:text-sm">workcation.com/</span>
-              <input type="text" name="username" id="username" autocomplete="username" class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="janesmith">
-            </div>
-          </div>
-        </div>
+  <div class="max-w-sm rounded overflow-hidden shadow-lg">
+    <div class="px-6 py-4">
+      <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
+      <p class="text-gray-700 text-base">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus
+        quia, nulla! Maiores et perferendis eaque, exercitationem praesentium
+        nihil.
+      </p>
+    </div>
+    <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+      <div class="mb-4">
+        <label class="block text-gray-700 text-sm font-bold mb-2" for="subject">
+          Subject
+        </label>
+        <input
+          class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          id="subject"
+          type="text"
+          placeholder="girl"
+          v-model="subject"
+        />
+        <p class="text-red-500 text-xs italic" v-if="error">display error.</p>
       </div>
-    </div>
-    </div>
-</form>
-    </div>
+      <div class="mb-6">
+        <label class="block text-gray-700 text-sm font-bold mb-2" for="verb">
+          Verb
+        </label>
+        <input
+          class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          id="verb"
+          type="text"
+          placeholder="read"
+          v-model="verb"
+        />
+        <p class="text-red-500 text-xs italic" v-if="error">display error.</p>
+      </div>
+      <div class="mb-6">
+        <label class="block text-gray-700 text-sm font-bold mb-2" for="object">
+          Object
+        </label>
+        <input
+          class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          id="object"
+          type="text"
+          placeholder="book"
+          v-model="object"
+        />
+        <p class="text-red-500 text-xs italic" v-if="error">display error.</p>
+      </div>
+      <div class="flex items-center justify-between">
+        <button
+          class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          type="button"
+        >
+          Cancel
+        </button>
+        <button
+          class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          type="button"
+        >
+          Submit
+        </button>
+      </div>
+    </form>
+  </div>
 </template>
 
 <script>
-// import { ref, reactive, computed, watch } from 'vue';
+import { ref } from "vue";
 
 export default {
-    name: 'CardInput',
-    setup() {
-        // Your setup logic here
+  name: "CardInput",
+  setup() {
+    // Your setup logic here
+    const error = ref(false);
+    const subject = ref("");
+    const verb = ref("");
+    const object = ref("");
 
-        return {
-            // Your data, methods, and computed properties here
-        };
-    },
+    return {
+      error,
+      subject,
+      verb,
+      object, // Your data, methods, and computed properties here
+    };
+  },
 };
 </script>
 
 <style scoped>
 /* Your component styles here */
+/* .error-text-input {
+  shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline
+} */
 </style>
